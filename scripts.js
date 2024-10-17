a90f7a466e5b7595a34a78b80cefd30fb181ecd5
iyannn03 <adryannask@gmail.com>
2024-10-17 21:59:22 +0700

    Add js toggle chat

--- scripts.js
+++ scripts.js
@@ -11,3 +11,9 @@ document.querySelectorAll('nav ul li a').forEach(anchor => {
 });
 
 // Optional: Add dynamic features like popups or carousel for reviews.
+const liveChatHeader = document.querySelector('#live-chat header');
+const chatWindow = document.querySelector('.chat');
+
+liveChatHeader.addEventListener('click', () => {
+    chatWindow.style.display = chatWindow.style.display === 'block' ? 'none' : 'block';
+});
